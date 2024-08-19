0c9e3a8cd96760cddb1a59c0e3349eb8e37112c5
Kenneth James Mendoza <messiahstudiosphx@gmail.com>
2024-08-19 02:50:52 -0700

    Update loadingScreen.js

--- loadingScreen.js
+++ loadingScreen.js
@@ -1,40 +1,34 @@
-window.addEventListener('load', function() {
+document.addEventListener('DOMContentLoaded', function() {
     const loadingScreen = document.getElementById('loadingScreen');
     const progressText = document.getElementById('progressText');
     loadingScreen.style.display = 'flex';
 
-    const totalDuration = 180 * 1000; // 180 seconds in milliseconds
-    const updateInterval = 1800;
     let currentPercentage = 0;
+    const updateInterval = 50; // Increase the percentage every 50ms
 
     // Function to update the percentage on the screen
     function updateProgress() {
-        if (currentPercentage <= 100) {
-            progressText.textContent = `Loading assets... ${currentPercentage}%`;
+        if (currentPercentage < 100) {
             currentPercentage++;
+            progressText.textContent = `Loading... ${currentPercentage}%`;
         } else {
             clearInterval(interval);
             hideLoadingScreen();
         }
     }
 
-    // Function to hide the loading screen without initiating app.js
+    // Function to hide the loading screen when the DOM is fully loaded
     function hideLoadingScreen() {
         loadingScreen.style.display = 'none';
         loadingScreen.remove();
     }
 
+    // Start updating the progress
     const interval = setInterval(updateProgress, updateInterval);
 
-    // Ensure that the loading screen is hidden once the DOM is fully loaded
-    document.addEventListener('DOMContentLoaded', () => {
-        clearInterval(interval);
+    // Check if the DOM is fully loaded and stop the loading screen
+    window.addEventListener('load', function() {
+        currentPercentage = 100;
         hideLoadingScreen();
     });
-
-    // As a fallback, hide the loading screen after the maximum duration
-    setTimeout(() => {
-        clearInterval(interval);
-        hideLoadingScreen();
-    }, totalDuration);
 });
